f82d1d65e1dac37f800a943ddcb0a8c97c0d217a
goyalira <161909542+goyalira@users.noreply.github.com>
2025-10-17 12:03:22 +0530

    Time: 119 ms (39.66%) | Memory: 20.2 MB (34.27%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int sum=0;
+        unordered_map<char,int>mp;
+        //creating substrings
+        for(int i=0;i<s.size();i++){
+            mp.clear();
+            for(int j=i;j<s.size();j++){
+                mp[s[j]]++;
+
+                int mini=INT_MAX;
+                int maxi=INT_MIN;
+                for(auto &each:mp){
+                    mini=min(mini,each.second);
+                    maxi=max(maxi,each.second);
+                }
+                int beauty=maxi-mini;
+                sum=sum+beauty;
+            }
+        }
+        return sum;
+    }
+};
