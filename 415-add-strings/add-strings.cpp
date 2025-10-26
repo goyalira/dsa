65ece42526c65fb8f55e20b781147d3f8bd7b73d
goyalira <161909542+goyalira@users.noreply.github.com>
2025-10-26 17:41:53 +0530

    Time: 71 ms (6.72%) | Memory: 182.9 MB (5.11%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
 
-    string addRE(string num1,int p1,string num2,int p2,int &carry){
+    void addRE(string num1,int p1,string num2,int p2,int &carry,string&ans){
         //base case
         if(p1<0 && p2<0){
             if(carry!=0)
-            return string(1,carry+'0');
-            return "";
+            ans.push_back(carry+'0');
+            return;
+            
         }
         //processing
         int n1=(p1>=0)?num1[p1]-'0':0;
@@ -14,18 +15,17 @@ public:
         int sum=n1+n2+carry;
         int digit=sum%10;
          carry=sum/10;
-        string ans="";
         ans.push_back(digit+'0');
         //recursive call
-        ans+=addRE(num1,p1-1,num2,p2-1,carry);
-        return ans;
+        addRE(num1,p1-1,num2,p2-1,carry,ans);
     }
 
     string addStrings(string num1, string num2) {
         int p1=num1.size()-1;
         int p2=num2.size()-1;
         int carry=0;
-        string ans=addRE(num1,p1,num2,p2,carry);
+        string ans="";
+        addRE(num1,p1,num2,p2,carry,ans);
         reverse(ans.begin(),ans.end());
         return ans;
     }
