77623a9b93b22dc790df0fab995d897ed5e6c448
goyalira <161909542+goyalira@users.noreply.github.com>
2026-03-12 00:14:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (23.65%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-         sort(s.begin(),s.end());
-         sort(t.begin(),t.end());
-        if(s==t)
+        int freqTable[256]={0};
+        for(int i=0;i<s.size();i++)
+        freqTable[s[i]]++;
+        for(int j=0;j<t.size();j++)
+        freqTable[t[j]]--;
+        for(int k=0;k<256;k++){
+            if(freqTable[k]!=0)
+            return false;
+        }
         return true;
-        else 
-        return false;
+        
     }
 };
